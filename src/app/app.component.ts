d026d7eacd5c2669655b2c7e3b6c3fdd9b69f812
LuisBueno2001 <luis.bueno@coppel.com>
2024-02-28 10:21:19 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AlumnosService } from './services/alumnos/alumnos.service';
+import { ClasesService } from './services/clases/clases.service';
+import { ClasesalumnosService } from './services/clasesalumnos/clasesalumnos.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,4 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FrondCrud';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, forwardRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule,Routes } from '@angular/router';
+import { FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { MainComponent } from './main/main.component';
+import { ClasesComponent } from './main/clases/clases.component';
+import { AlumnosComponent } from './main/alumnos/alumnos.component';
+import { ClasesAlumnoComponent } from './main/clases-alumno/clases-alumno.component';
+
+const routes: Routes=[
+  {path:'', redirectTo:'', pathMatch:'full'},
+  {path:'main', component:MainComponent},
+  {path:'clases', component:ClasesComponent},
+  {path: 'clases/:id', component:ClasesComponent},
+  {path:'alumno', component:AlumnosComponent},
+  {path: 'alumno/:id', component:AlumnosComponent},
+  {path: 'clasesalumno', component:ClasesAlumnoComponent},
+  {path: 'clasesalumno/:id', component:ClasesAlumnoComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ClasesComponent,
+    AlumnosComponent,
+    ClasesAlumnoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ClasesAlumnoComponent),
+      multi: true,
+    }
+  ],
+  bootstrap: [AppComponent],
+  schemas:[
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
